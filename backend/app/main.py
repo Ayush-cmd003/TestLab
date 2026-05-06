4122beb1da6fc54404a957ae6cd6794c9f656f15
Ayush <79743568+Ayush-cmd003@users.noreply.github.com>
2026-05-06 23:21:06 +0530

    Removed trailing slash at the end from api url in cors origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://testlab-workspace.vercel.app/"
+        "https://testlab-workspace.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
